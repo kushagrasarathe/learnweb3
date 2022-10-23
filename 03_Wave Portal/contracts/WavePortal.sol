064f92fd46e6dbc4c3ab6250fd3c4d789e27a772
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-10-23 22:05:38 +0530

    wave function added

--- 03_Wave Portal/contracts/WavePortal.sol
+++ 03_Wave Portal/contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
     constructor() {
-        console.log("I'am a smart-contract");
+        console.log("This is a wave portal contract");
     }
+    
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved",  msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d waves!!!",  totalWaves);
+        return totalWaves;
+    }
+
 }
