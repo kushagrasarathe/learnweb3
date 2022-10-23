57b196f36eeb18bdda5266f0423bce787a5ab4f1
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-10-23 22:14:51 +0530

    deploy script updated

--- 03_Wave Portal/scripts/run.js
+++ 03_Wave Portal/scripts/run.js
@@ -1,22 +1,35 @@
 const main = async () => {
-  const waveContractFactory = await hre.ethers.getContractFactory(
-    "WavePortal"
-  );
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log("Contract deployed to: ", waveContract.address);
+  console.log("Contract deployed by: ", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const firstWaveTxn = await waveContract.wave();
+  await firstWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
+
+  const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+  await secondWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
-    try {
-        await main()
-        process.exit(0)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-runMain()
+runMain();
 
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
